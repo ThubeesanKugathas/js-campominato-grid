717fc9527ece8d427a1eaffd8c34af74176742dc
ThubeesanKugathas <Thubeesan@outlook.com>
2021-12-17 14:18:30 +0100

    simple 3 buttons fuctions js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 // definisco la funzione del creare le box
-function createBox(boxContainer) {
+function createBox1(boxContainer) {
     const boxHtml = document.createElement('div');
     boxHtml.className = 'ms_box-1';
-    boxContainer.append(boxHtml);
 
+    boxContainer.append(boxHtml);
+    boxHtml.addEventListener('click', function() {
+        this.classList.toggle('ms_color');
+    })
+}
+
+function createBox2(boxContainer) {
+    const boxHtml = document.createElement('div');
+    boxHtml.className = 'ms_box-2';
+
+    boxContainer.append(boxHtml);
+    boxHtml.addEventListener('click', function() {
+        this.classList.toggle('ms_color');
+    })
+}
+
+function createBox3(boxContainer) {
+    const boxHtml = document.createElement('div');
+    boxHtml.className = 'ms_box-3';
+
+    boxContainer.append(boxHtml);
     boxHtml.addEventListener('click', function() {
         this.classList.toggle('ms_color');
     })
@@ -12,14 +32,25 @@ function createBox(boxContainer) {
 // definisco l'output del div container
 let containerHtml = document.querySelector('.ms_box-container');
 
-// rendere una funzione così da non ripetere il ciclo TODO!
-for (let i = 0; i < 100; i++) {
-    // outputHtml.innerHTML += '<div class="box"></div>';
-    const newBox = createBox(containerHtml);
-}
-
 // definisco gli output dei button
-const button1 = document.getElementById(ms_button1);
-const button2 = document.getElementById(ms_button2);
-const button3 = document.getElementById(ms_button3);
+const button1 = document.getElementById('ms_button1');
+const button2 = document.getElementById('ms_button2');
+const button3 = document.getElementById('ms_button3');
 
+button1.addEventListener('click', function () {
+    for (let i = 0; i < 100; i++) {
+        const newBox = createBox1(containerHtml);
+    }
+})
+
+button2.addEventListener('click', function () {
+    for (let i = 0; i < 81; i++) {
+        const newBox = createBox2(containerHtml);
+    }
+})
+
+button3.addEventListener('click', function () {
+    for (let i = 0; i < 49; i++) {
+        const newBox = createBox3(containerHtml);
+    }
+})
